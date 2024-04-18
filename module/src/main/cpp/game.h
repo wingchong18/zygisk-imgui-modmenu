234e519252d979218c9050e4a784e0c2cfe5350a
wingchong18 <167405733+wingchong18@users.noreply.github.com>
2024-04-18 14:03:07 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.netease.idv.googleplay"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
