a3b69063d32c1e378e269be567f0e59bf002600d
wingchong18 <167405733+wingchong18@users.noreply.github.com>
2024-04-20 06:44:41 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -2,7 +2,7 @@
 #define ZYGISK_IMGUI_MODMENU_GAME_H
 
 // TODO: change this
-#define TargetLibName "libil2cpp.so"
+#define TargetLibName "libGLESv3.so"
 #define GamePackageName "com.netease.idv.googleplay"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
